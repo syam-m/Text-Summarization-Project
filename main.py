7fce3abfe24582ddab707d1e8320ce70ec9bb96d
syam-m <mungisyam1@gmail.com>
2024-01-10 06:12:15 -0500

    data validation scripts done

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tsp.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from tsp.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 
 from tsp.logging import logger
-
-
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -11,6 +11,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/tsp/pipeline/stage_01_data_ingestion.py
+++ src/tsp/pipeline/stage_01_data_ingestion.py
@@ -4,11 +4,13 @@ from tsp.logging import logger
 
 
 
-try:
-    config = ConfigurationManager()
-    data_ingestion_config = config.get_data_ingestion_config()
-    data_ingestion = DataIngestion(config=data_ingestion_config)
-    data_ingestion.download_file()
-    data_ingestion.extract_zip_file()
-except Exception as e:
-    raise e
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
